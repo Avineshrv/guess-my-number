2860fff9fdacb83808dafdea99949c9e62c2ec6b
Avineshrv <48782992+Avineshrv@users.noreply.github.com>
2024-07-07 13:43:58 +0530

    dom style manipulation done

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
 console.log(secretNumber);
 const inputGuess = document.querySelector('.guess');
 const checkButton = document.querySelector('.check');
@@ -13,7 +12,10 @@ checkButton.addEventListener('click', function () {
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number';
   } else if (guess === secretNumber) {
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('.message').textContent = 'Correct Number';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High';
